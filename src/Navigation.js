ba063e7390ba740260a5458e7b6d4cd3057e0331
Kendall Howard <kenxh@email.unc.edu>
2021-03-27 16:35:42 -0400

    fixed nav bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Navigation = () => {
     return (
        <div>
-          <a target ="todo" href="https://kenxh.github.io/todo-app/">To Do App </a>
-          <a target ="endpoint" href="https://kenxh.github.io/endpoint-app/">Endpoint App </a>
-          <a target ="notes" href="https://kenxh.github.io/note-app/">Notes App </a>
+          <a target ="_blank" href="https://kenxh.github.io/todo-app/">To Do App </a>
+          <a target ="_blank" href="https://kenxh.github.io/endpoint-app/">Endpoint App </a>
+          <a target ="_blank" href="https://kenxh.github.io/note-app/">Notes App </a>
        </div>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HomePage from "../pages/homePage";
+import ToDoApp from "../pages/toDoApp";
+import EndpointApp from "../pages/endpointApp";
+import NotesApp from "../pages/notesApp";
 import Navigation from "../Navigation"
 
 
@@ -10,7 +14,15 @@ const Header = () => (
         <Link to="/">
           <h1 className="site-title">Kendall's Website</h1>
         </Link>
-        <Navigation></Navigation>
+        <Router>
+          <div>
+            <Navigation>
+              <Route exact path="/" component={ToDoApp} />
+              <Route exact path="/" component={EndpointApp} />
+              <Route exact path="/" component={NotesApp} />
+            </Navigation>
+          </div>
+        </Router>
       </div>
     </div>
   </header>
